0f46f10239558b6f1beb5b42eb87f219a46a391c
Samee Zahur <samee.zahur@gmail.com>
2021-12-17 16:52:30 -0800

    Fewer integer casts

--- 15/part2.rs
+++ 15/part2.rs
@@ -1,28 +1,28 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
+use std::convert::TryInto;
 use std::io::Read;
 
+// Learnt about .wrapping_sub()
+// Also had to bias dr,dc to prevent wraparound, explicitly relying on it.
 fn read_input() -> Vec<u8> {
   let mut bytes = Vec::new();
   std::io::stdin().read_to_end(&mut bytes).unwrap();
   return bytes;
 }
 
-fn board_cost(board: &[&[u8]], r: isize, c: isize) -> u32 {
-  let rows = board.len() as u32;
-  let cols = board[0].len() as u32;
-  let r1 = r as u32/rows;
-  let r2 = r as u32%rows;
-  let c1 = c as u32/cols;
-  let c2 = c as u32%cols;
-  let cost1 = (board[r2 as usize][c2 as usize] - b'0') as u32;
-  return (cost1 - 1 + r1 + c1)%9 + 1;
+fn board_cost(board: &[&[u8]], r: usize, c: usize) -> u32 {
+  let rows = board.len();
+  let cols = board[0].len();
+  let cost_shift : u32 = (r/rows + c/cols).try_into().unwrap();
+  let cost1 : u32 = (board[r%rows][c%cols] - b'0').try_into().unwrap();
+  return (cost1 - 1 + cost_shift)%9 + 1;
 }
 
 fn shortest_path(board: &[&[u8]]) -> u32 {
-  let rows = 5*board.len() as isize;
-  let cols = 5*board[0].len() as isize;
-  let mut visited = vec![vec![false; cols as usize]; rows as usize];
+  let rows = 5*board.len();
+  let cols = 5*board[0].len();
+  let mut visited = vec![vec![false; cols]; rows];
   let mut q = BinaryHeap::new();
   q.push((Reverse(0u32), 0, 0));
   while !q.is_empty() {
@@ -30,13 +30,13 @@ fn shortest_path(board: &[&[u8]]) -> u32 {
       Some(p) => p,
       None => panic!("Should be empty"),
     };
-    if visited[r as usize][c as usize] { continue; }
-    visited[r as usize][c as usize] = true;
-    if r == rows-1 && c == cols-1 { return dist; }
-    for (dr, dc) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
-      let r2 = r+dr;
-      let c2 = c+dc;
-      if r2 < 0 || r2 >= rows || c2 < 0 || c2 >= cols { continue; }
+    if visited[r][c] { continue; }
+    visited[r][c] = true;
+    if r+1 == rows && c+1 == cols { return dist; }
+    for (dr, dc) in [(0,1), (1,2), (2,1), (1,0)] {
+      let r2 = (r+dr).wrapping_sub(1);
+      let c2 = (c+dc).wrapping_sub(1);
+      if r2 >= rows || c2 >= cols { continue; }
       let dd = board_cost(board, r2, c2);
       q.push((Reverse(dist+dd), r2, c2));
     }
